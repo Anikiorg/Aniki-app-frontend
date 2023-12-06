cdefeca1a356aac46c50aedf8d1b45dc3cc49515
Gustavo Fonte <gusfonte99@gmail.com>
2023-12-06 15:18:25 +0000

    fix http request looping

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 function AnimeList() {
     const [animeList, setAnimeList] = useState([])
-    axios.get("/api/animes")
-        .then((response) => {
-            setAnimeList(response.data)
-        })
-        .catch((error) => console.log(error))
-        console.log("hello")
-        console.log(animeList);
+    
+    useEffect(() => {
+        axios.get("http://localhost:5005/api/animes")
+            .then((response) => {
+                setAnimeList(response.data)
+                console.log(animeList);
+            })
+            .catch((error) => console.log(error))
+            console.log("hello")
+    }, [])
     
         return (
             <>
-            hi
         {animeList.map((anime)=> {
-            return <>
-            <h1>inside the map</h1>
-            <h1>{anime.name}</h1>
+            return <div key={anime._id}>
+            <h1>Anime</h1>
+            <h1>{anime.name.nameJP}</h1>
             <p>{anime.imageURL}</p>
             <p>{anime.genre}</p>
             <p>{anime.rating}</p>
-            </>
+            </div>
         })}
     </>
         )
