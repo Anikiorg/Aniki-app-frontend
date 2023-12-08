3ae4e5bc281397de5ae2a07d51cbcc94945dd30e
Gustavo Fonte <gusfonte99@gmail.com>
2023-12-08 01:52:28 +0000

    Fixes/changes/formatting to create anime form

--- src/components/CreateAnime/CreateAnime.jsx
+++ src/components/CreateAnime/CreateAnime.jsx
@@ -34,13 +34,13 @@ function CreateAnime () {
     }
         
     const handleSubmit = (e) => {
-        e.preventDefault()
+        /* e.preventDefault() */
 
         axios.post("http://localhost:5005/api/animes", createdAnime)
         .then(() => {
             console.log(createdAnime)
             console.log("Anime created")
-            navigate("/")
+            /* navigate("/animes") */
         })
         .catch((err) => console.log(err))
     }
@@ -48,22 +48,22 @@ function CreateAnime () {
 return (
     <form onSubmit={handleSubmit}>
         <label>Japanese name: </label>
-        <input type="text" placeholder="Enter japanese title" name="nameJP" value={nameJP} onChange={(e) => {setNameJP(e.target.value)}}/><br/>
+        <input type="text" placeholder="Enter japanese title" name="nameJP" value={nameJP} onChange={(e) => setNameJP(e.target.value)}/><br/>
         
         <label>English name: </label>
-        <input type="text" placeholder="Enter english title" name="nameEN" value={nameEN} onChange={(e) => {setNameEN(e.target.value)}}/><br/>
+        <input type="text" placeholder="Enter english title" name="nameEN" value={nameEN} onChange={(e) => setNameEN(e.target.value)}/><br/>
         
         <label>Description: </label>
-        <textarea placeholder="Enter description" name="description" value={description} onChange={(e)=> {setDescription(e.target.value)}}/><br/>
+        <textarea placeholder="Enter description" name="description" value={description} onChange={(e)=> setDescription(e.target.value)}/><br/>
         
         <label>ImageURL: </label>
-        <input type="text" placeholder="Add imageURGL" name="imageURL" value={imageURL} onChange={(e) => { setImageURL(e.target.value)}}/><br/>
+        <input type="text" placeholder="Add imageURGL" name="imageURL" value={imageURL} onChange={(e) => setImageURL(e.target.value)}/><br/>
         
         <label>Episodes: </label>
-        <input type="number" placeholder="Enter number of episodes" mame="episodes" value={episodes} onChange={(e) => {setEpisodes(e.target.value)}}/><br/>
+        <input type="number" placeholder="Enter number of episodes" mame="episodes" value={episodes} onChange={(e) => setEpisodes(e.target.value)}/><br/>
         
         <label>Genre: </label>
-        <select name="genre" value={genre} onChange={(e) => {setGenre(e.target.value)}}>
+        <select name="genre" value={genre} onChange={(e) => setGenre(e.target.value)}>
             <option>-- Select genre --</option>
             <option>Comedy</option>
             <option>Slice of life</option>
@@ -73,7 +73,7 @@ return (
         <br/>
         
         <label>Status: </label>
-        <select name="status" value={status} onChange={(e)=>{setStatus(e.target.value)}}>
+        <select name="status" value={status} onChange={(e)=> setStatus(e.target.value)}>
             <option>-- Select status --</option>
             <option>Airing Now</option>
             <option>Finished Airing</option>
@@ -83,14 +83,14 @@ return (
         <br/>
         
         <label>Premiered: </label>
-        <input type="date" name="premiered" value={premiered} onChange={(e)=> {setPremiered(e.target.value)}} /><br/>
+        <input type="text" name="premiered" value={premiered} onChange={(e)=> setPremiered(e.target.value)} /><br/>
         
         <label>Studios: </label> 
-        <input type="text" placeholder="Add producing studios" name="studios" value={studios} onChange={(e)=> {setStudios(e.target.value)}}/><br/>
+        <input type="text" placeholder="Add producing studios" name="studios" value={studios} onChange={(e)=> setStudios(e.target.value)}/><br/>
         
         <label>Rating: </label>
-        <input type="number" placeholder="Add rating" name="rating" value={rating}onChange={(e) => {setRating(e.target.value)}}/><br/>
-        <select name="ageRating" value={ageRating} onChange={(e)=> {setAgeRating(e.target.value)}}>
+        <input type="number" placeholder="Add rating" name="rating" value={rating}onChange={(e) => setRating(e.target.value)}/><br/>
+        <select name="ageRating" value={ageRating} onChange={(e)=> setAgeRating(e.target.value)}>
         <option>-- Add age rating --</option>
         <option>E - Everyone / A - All Ages</option>
         <option>T - Teens, Age 13+</option>
@@ -99,8 +99,6 @@ return (
         </select>
         
         <br/>
-        
-        <label>Rating:</label> {/* WHY??????????????????????????? */}
         
         <button type="submit">Submit</button>
     </form>
