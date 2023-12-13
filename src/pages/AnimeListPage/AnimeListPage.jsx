aff64fb87ab96d6a55949d1216de0548ceaf6d1e
Zohra <zohra.boukhatem1@gmail.com>
2023-12-13 17:25:55 +0100

    fix filter for anime list

--- src/pages/AnimeListPage/AnimeListPage.jsx
+++ src/pages/AnimeListPage/AnimeListPage.jsx
@@ -4,19 +4,22 @@ import { Link } from "react-router-dom";
 import AnimeCreate from "../../components/Anime/AnimeCreate";
 import AddToList from "../../components/Lists/AnimeLists/AddToList";
 import { AuthContext } from "../../context/auth.context";
+
 function AnimeListPage() {
   const [animeList, setAnimeList] = useState([]);
   const [animeBackup, setAnimeBackup] = useState([]);
+  const [searchedList, setSearchedList] = useState([])
   const [toggle, setToggle] = useState(false);
   const [input, setInput] = useState("");
   const { user } = useContext(AuthContext);
 
-
-  
+  //TOGGLE FOR ADD ANIME
   const handleToggle = () => {
     setToggle(!toggle);
   };
 
+
+  //SET ANIME LIST AND BACKUP WITH .GET
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/api/animes`)
@@ -29,25 +32,9 @@ function AnimeListPage() {
   }, []);
 
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setInput(e.target.value);
-  };
-  
-  useEffect(() => {
-    const filtered = [...animeBackup].filter((current) =>
-      input
-        ? current.name.nameEN.toLowerCase().includes(input.toLowerCase()) ||
-          current.name.nameJP.toLowerCase().includes(input.toLowerCase())
-        : current
-    );
-    setAnimeList(filtered);
-  }, [input]);
-
-
+// SETS ANIMELIST TO FILTERED ANIME (BY GENRE) AND RENDERS IT
+let filteredArray;
   const handleSelect = (e) => {
-    console.log(e.target.value);
-    let filteredArray;
     e.preventDefault();
     switch (e.target.value) {
       case "All Anime":
@@ -59,7 +46,6 @@ function AnimeListPage() {
         });
         setAnimeList(filteredArray);
         break;
-
       case "Comedy":
         filteredArray = animeBackup.filter((anime) => {
           return anime.genre.includes("Comedy");
@@ -70,35 +56,70 @@ function AnimeListPage() {
         filteredArray = animeBackup.filter((anime) => {
           return anime.genre.includes("Slice of life");
         });
-        setAnimeList(filteredArray)
+        setAnimeList(filteredArray);
         break;
     }
   };
 
+
+
+//SET SEARCH BAR INPUT AND PREVENT DEFAULT
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+  
+  // IN CASE OF INPUT SEARCH INSIDE FILTERED ANIMELIST, SET SEARCHED LIST TO MATCHING ANIME
+    useEffect(() => {
+      const filtered = [...animeList].filter((current) =>
+        input
+          ? current.name.nameEN.toLowerCase().includes(input.toLowerCase()) ||
+            current.name.nameJP.toLowerCase().includes(input.toLowerCase())
+          : current
+      );
+      setSearchedList(filtered);
+    }, [input, animeList]);
+    
+
+
+
+
   return (
     <>
-      {(user && user.typeOfUser === "admin") && <button onClick={handleToggle}>Add anime</button>}
-      {toggle && <AnimeCreate />}
+
+      {/*TOGGLE ADD ANIME FORM*/}
+    
+        {user && user.typeOfUser === "admin" && (
+          <button onClick={handleToggle}>Add anime</button>
+          )}
+        {toggle && <AnimeCreate />}
+        <br />
+ 
+
+      {/*SELECT FOR CATEGORIES, CALLS HANDLE SELECT */}
+        <label>Filter by genre:</label>
+        <select onChange={handleSelect}>
+          <option value="All Anime">All Anime</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Slice of life">Slice of life</option>
+          <option value="Action">Shounen</option>
+        </select>
       <br />
+
+
+
+          {/*SEARCH BAR*/}
       <input
         type="text"
         placeholder="Search here"
         onChange={(e) => handleChange(e)}
         value={input}
-      />
+        />
       <br />
 
-      <label>Filter by genre:</label>
-      <select onChange={handleSelect}>
-        <option value="All Anime">All Anime</option>
-        <option value="Comedy">Comedy</option>
-        <option value="Slice of life">Slice of life</option>
-        <option value="Action">Shounen</option>
-      </select>
-      <br />
-
+      {/*RENDER FILTERED AND SEARCHED ANIME + ADD TO LIST + DETAILS PAGE*/}
       <>
-        {animeList.map((anime) => {
+        {searchedList.map((anime) => {
           return (
             <div key={anime._id}>
               <h1>Anime</h1>
