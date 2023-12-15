85fda45b7408e03fb7fee9321db906029cf5afd5
Gustavo Fonte <gusfonte99@gmail.com>
2023-12-15 02:04:28 +0000

    Initial css for homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,19 +3,74 @@ import {Link} from "react-router-dom"
 
 function HomePage() {
   return (
-      <>
-    <div>
-      <h1>Home page</h1>
+    <div className="">
+
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="36"
+                height="36"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-chevron-right"
+              >
+                <path d="m9 18 6-6-6-6" />
+              </svg>
+            </a>
+            <a href="#slide2" className="btn btn-circle"></a>
+          </div>
+        </div>
+
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle"></a>
+            <a href="#slide3" className="btn btn-circle"></a>
+          </div>
+        </div>
+
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle"></a>
+            <a href="#slide4" className="btn btn-circle"></a>
+          </div>
+        </div>
+
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle"></a>
+            <a href="#slide1" className="btn btn-circle"></a>
+          </div>
+        </div>
+      </div>
+
+      <Link to="/browse">
+        <button className="btn">Browse</button>
+      </Link>
+
     </div>
-    
-    <Link to="/animes" >
-      <button className="btn">All Anime</button>
-    </Link>
-    
-    <Link to="/manga" >
-      <button className="btn">All Manga</button>
-    </Link>
-    </>
   );
 }
 
