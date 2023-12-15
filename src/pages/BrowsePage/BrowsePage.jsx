f8bd1c71e6fadc7092dee21cdca2c88012d590a8
Gustavo Fonte <gusfonte99@gmail.com>
2023-12-15 02:01:46 +0000

    Add browse page

---
+++ src/pages/BrowsePage/BrowsePage.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import AnimeListPage from "../AnimeListPage/AnimeListPage";
+import MangaListPage from "../MangaListPage/MangaListPage";
+
+function BrowsePage() {
+  const [isAnime, setIsAnime] = useState(true);
+  
+  const handleContentType = () => {
+    console.log("clicked");
+    setIsAnime(!isAnime);
+  };
+
+  return (
+    <div>
+    <button className="btn" onClick={() => {handleContentType()}}>{isAnime ? <p>Anime</p> : <p>Manga</p>}</button>
+
+    {isAnime && <AnimeListPage/> || !isAnime && <MangaListPage/>}
+    </div>
+  );
+}
+
+export default BrowsePage
